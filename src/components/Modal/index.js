321b62184587a719aba304762e68c6e0867607c4
Aaron Lisker <aelisker@gmail.com>
2020-07-21 17:12:03 -0700

    tests for modal successful

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Modal({currentPhoto}) {
+const Modal = ({onClose, currentPhoto}) => {
   const {name, category, description, index} = currentPhoto;
 
   return(
@@ -12,7 +12,10 @@ function Modal({currentPhoto}) {
           alt="current category" 
         />
         <p>{description}</p>
-        <button type="button">Close this modal</button>
+        <button 
+          type="button" 
+          onClick={onClose}
+        >Close this modal</button>
       </div>
     </div>
   );
